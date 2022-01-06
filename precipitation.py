30a6dd85bc474e7d546e12788c8f25a42a8cb99a
shubham shankar <msgtoshubham@gmail.com>
2022-01-06 17:16:24 +0530

    added a readme

---
+++ precipitation.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+train_set = pd.read_csv('precipitation.csv')
+train_set.head(5)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+In this project we develop a model to predict
+whether it will rain on a particular day
