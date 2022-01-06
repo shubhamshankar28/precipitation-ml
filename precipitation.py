fe6cb91cb979f26a49d989b80b313ca38c0edb26
shubham shankar <msgtoshubham@gmail.com>
2022-01-06 17:41:11 +0530

    replaced appropriate values

--- precipitation.py
+++ precipitation.py
@@ -2,5 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+#print("executing the lengthy part")
 train_set = pd.read_csv('precipitation.csv')
-train_set.head(5)
+## replace > 0 with 1
+train_set[(train_set['PRCP'] > 0)] = 1
